6c3268c555be3bd95b958312654a324726651e64
Lucas Almeida <lucasnopc@gmail.com>
2022-01-03 11:42:44 -0300

    separando header em componentes menores

---
+++ src/components/layouts/home/header/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
+import { format } from "date-fns"
+import Logo from '../logo'
+import SportNav from '../sportsnav'
+import Profile from './profile'
+
+export default function Header(props) {
+    let user = ""
+    const tzid = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const datetoday = `${format(new Date(), `dd.MM.yy`)} - ${tzid}`
+    const [session] = useSession()
+    const router = useRouter()
+   
+    if (props.userString) {
+        user = JSON.parse(props.userString)
+    }
+
+    return <>
+        <header className=" bg-green-600 border-b border-gray-200  absolute top-0 left-0 w-screen z-30 md:h-16 md:overflow-hidden">
+            <div className='pl-2 md:p-3 flex items-center justify-between'>
+            <Logo />
+            <div id="profile">
+            {session && <><div id="points" className="inline-block h-full mr-5">
+                    <span className="bgicon-coin align-middle"></span>
+                    <span className="ml-2 font-medium text-xs align-middle text-white">R$ {user ? Number(user.points).toFixed(2) : `0.00`}</span>
+                </div>
+                </>}
+                <Profile />
+            </div>
+            </div>
+        <SportNav />
+        </header>
+    </>
+}
