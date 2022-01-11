475008a36a5c5430fd0cdaf378cfeb242deb0761
Lucas Almeida <lucasnopc@gmail.com>
2022-01-11 17:15:09 -0300

    mudando key

--- src/pages/finances/index.jsx
+++ src/pages/finances/index.jsx
@@ -6,24 +6,28 @@ import AllPays from './allpays'
 import { useMercadopago } from 'react-sdk-mercadopago';
 
 export default function Finances(props) {
-  const stringPublicKey = String(process.env.MP_PRO_PUBLIC_KEY)
-  const mercadopago = useMercadopago.v2(stringPublicKey, {
-    locale: 'pt-BR'
-});
+  const stringPublicKey = 'APP_USR-9bee11b4-7b73-4936-8610-9cfa6797e650'
+  let mercadopago = undefined
+  if (stringPublicKey) {
+    mercadopago = useMercadopago.v2(stringPublicKey, {
+      locale: 'pt-BR'
+    });
+  }
+
 
   let user = ``
-  if(props.userString) {
+  if (props.userString) {
     user = JSON.parse(props.userString)
   }
 
   const deposit = e => {
+    console.log('stringPublicKey', stringPublicKey)
     e.preventDefault()
     const orderData = {
       quantity: e.target[0].value,
       description: `${e.target[0].value} Pontos - Betbol`,
       price: 1
     }
-
 
     fetch("/api/mp", {
       method: "POST",
@@ -37,7 +41,7 @@ export default function Finances(props) {
       })
       .then(function (preference) {
         if (mercadopago) {
-          const checkout = mercadopago.checkout({
+          mercadopago.checkout({
             preference: {
               id: preference.id
             },
@@ -52,25 +56,24 @@ export default function Finances(props) {
       <Head>
         <title>Betbol - Financeiro</title>
         <link rel="icon" href="/favicon.ico" />
-        {/* <script src="https://sdk.mercadopago.com/js/v2"></script> */}
       </Head>
 
       <Layout userString={props.userString}>
-          <div className="mx-3 mt-3 md:col-span-7 col-span-full bg-white shadow-md">
-            <h2 className="page-title border-b border-gray-100">Escolha o valor do depósito</h2>
-            <span className="p-2">Defina o valor do seu depósito a partir de <b>R$10,00</b></span>
-            <div className="p-2">
-              <form onSubmit={e => deposit(e)}>
-                R$ <input type="number" name="valorDeposit" step="10" className="border border-gray-300 active:outline-none outline-none text-2xl w-28 text-green-800" min="0" />
-                <input type="submit" className="inline-block cursor-pointer transform -translate-y-0.5 bg-primary-ligth hover:bg-primary p-2 text-sm font-semibold text-white ml-1" value="APLICAR" />
-              </form>
-              <div id="paym">
-                Todos pagamentos
-                <AllPays user={user} mercadopago={mercadopago} />
-              </div>
-
+        <div className="mx-3 mt-3 md:col-span-7 col-span-full bg-white shadow-md">
+          <h2 className="page-title border-b border-gray-100">Escolha o valor do depósito</h2>
+          <span className="p-2">Defina o valor do seu depósito a partir de <b>R$10,00</b></span>
+          <div className="p-2">
+            <form onSubmit={e => deposit(e)}>
+              R$ <input type="number" name="valorDeposit" step="10" className="border border-gray-300 active:outline-none outline-none text-2xl w-28 text-green-800" min="0" />
+              <input type="submit" className="inline-block cursor-pointer transform -translate-y-0.5 bg-primary-ligth hover:bg-primary p-2 text-sm font-semibold text-white ml-1" value="APLICAR" />
+            </form>
+            <div id="paym">
+              Todos pagamentos
+              <AllPays user={user} mercadopago={mercadopago} />
             </div>
+
           </div>
+        </div>
       </Layout>
     </>
   )
