e957023c1e80f60daf1e0d8203b38581aa64ce34
Lucas Almeida <lucasnopc@gmail.com>
2022-03-08 12:32:41 -0300

    fix bug build

--- src/pages/user/deposit/QrcodeComponent/index.jsx
+++ src/pages/user/deposit/QrcodeComponent/index.jsx
@@ -7,7 +7,7 @@ import useUser from '../../../../utills/hooks/useUser'
 import axios from 'axios';
 
 export default function QrcodeComponent({ qrCode, valueDeposit, setQrCode, txid, user }) {
-  const [isCopied, setCopied] = useCopyClipboard(qrCode.qrcode);
+  const [isCopied, setCopied] = useCopyClipboard("");
   const [confirmationPix, setConfirmationPix] = useState(null)
   const { data, error } = useFetch(`/api/pix/getpix?txid=${txid}`, { refreshInterval: 1000 });
   if (error) console.log(error)
@@ -44,16 +44,18 @@ export default function QrcodeComponent({ qrCode, valueDeposit, setQrCode, txid,
   }, [confirmationPix])
   console.log(confirmationPix)
   const copiedPix = () => {
-    setCopied(qrCode.qrcode)
-    toast.success('PIX Copiado', {
-      position: "top-right",
-      autoClose: 3000,
-      hideProgressBar: true,
-      closeOnClick: false,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    })
+    if(qrCode.qrcode) {
+      setCopied(qrCode?.qrcode)
+      toast.success('PIX Copiado', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    }
   }
   const verifyPayment = () => {
     console.log('verify')
@@ -64,7 +66,7 @@ export default function QrcodeComponent({ qrCode, valueDeposit, setQrCode, txid,
       <h2 className='font-semibold text-base text-center'>Faça o pagamento de R$ {Number(valueDeposit).toFixed(2)} via Pix com o qrcode abaixo</h2>
       <span className='w-full text-center font-semibold text-primary cursor-pointer' onClick={() => copiedPix()}>Clique para copiar o código de pagamento pix</span>
       <div className='flex justify-center items-center'>
-        <span onClick={() => copiedPix()} className="cursor-pointer"><img src={qrCode.imagemQrcode} className="min-h-full" /></span>
+        <span onClick={() => copiedPix()} className="cursor-pointer"><img src={qrCode?.imagemQrcode} className="min-h-full" /></span>
       </div>
       <span className='text-center'>Após realizar o pix, clique em <span className='text-blue-600 hover:text-blue-700 font-semibold cursor-pointer' onClick={() => verifyPayment()}>Verificar Pagamento</span></span>
       <span className='text-red-600 hover:text-red-700 text-center font-semibold cursor-pointer' onClick={() => setQrCode({})}>[x] fechar</span>
