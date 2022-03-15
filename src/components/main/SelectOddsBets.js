28390702bfcde6e8942e2823a92376ab70e250f9
Lucas Almeida <lucasnopc@gmail.com>
2022-03-15 13:43:12 -0300

    layout select bets

--- src/components/main/SelectOddsBets.js
+++ src/components/main/SelectOddsBets.js
@@ -2,7 +2,7 @@ import { oddBets } from '../../utills/oddBets'
 
 export default function SelectOddsBets(props) {
     return <div className="inline-block md:float-right">
-            <select className="font-semibold overflow-auto bg-gray-50 uppercase hover:bg-gray-100 float-right border border-gray-100 p-1 mr-2 outline-none cursor-pointer w-full text-gray-700" onChange={(change) => {
+            <select className="font-semibold overflow-auto text-right bg-gray-50 uppercase hover:bg-gray-100 float-right border border-gray-100 p-1 mr-2 outline-none cursor-pointer w-full text-gray-700 text-sm" onChange={(change) => {
                 props.setBets(change.target.value)
             }}>
                 {oddBets.map((bet, i) => {
