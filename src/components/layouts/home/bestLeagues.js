732c0f313997c54e5feb516de04a403ae962da02
Lucas Almeida <lucasnopc@gmail.com>
2022-04-03 20:10:15 -0300

    upgrade bestLEagues

--- src/components/layouts/home/bestLeagues.js
+++ src/components/layouts/home/bestLeagues.js
@@ -1,5 +1,83 @@
 export const bestLeagues = [
     {
+        "id": 1,
+        "name": "World Cup",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/1.png"
+    },
+    {
+        "id": 2,
+        "name": "UEFA Champions League",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/2.png"
+    },
+    {
+        "id": 3,
+        "name": "UEFA Europa League",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/3.png"
+    },
+    {
+        "id": 4,
+        "name": "Euro Championship",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/4.png"
+    },
+    {
+        "id": 5,
+        "name": "UEFA Nations League",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/5.png"
+    },
+    {
+        "id": 8,
+        "name": "World Cup - Women",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/8.png"
+    },
+    {
+        "id": 9,
+        "name": "Copa America",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/9.png"
+    },
+    {
+        "id": 15,
+        "name": "FIFA Club World Cup",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/15.png"
+    },
+    {
+        "id": 20,
+        "name": "CAF Confederation Cup",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/20.png"
+    },
+    {
+        "id": 21,
+        "name": "Confederations Cup",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/21.png"
+    },
+    {
+        "id": 38,
+        "name": "UEFA U21 Championship",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/38.png"
+    },
+    {
+        "id": 34,
+        "name": "World Cup - Qualification South America",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/34.png"
+    },
+    {
+        "id": 490,
+        "name": "World Cup - U20",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/490.png"
+    },
+    {
         "id": 71,
         "name": "Brasileirão Serie A",
         "type": "League",
@@ -12,10 +90,136 @@ export const bestLeagues = [
         "logo": "https:\/\/media.api-sports.io\/football\/leagues\/72.png"
     },
     {
+        "id": 73,
+        "name": "Copa Do Brasil",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/73.png"
+    },
+    {
+        "id": 74,
+        "name": "Brasileiro Women",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/74.png"
+    },
+    {
+        "id": 476,
+        "name": "Paulista - A2",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/476.png"
+    },
+    {
+        "id": 521,
+        "name": "Amapaense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/521.png"
+    },
+    {
+        "id": 522,
+        "name": "Amazonense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/522.png"
+    },
+    {
+        "id": 76,
+        "name": "Serie D",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/76.png"
+    },
+    {
+        "id": 520,
+        "name": "Acreano",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/520.png"
+    },
+    {
+        "id": 77,
+        "name": "Alagoano",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/77.png"
+    },
+    {
+        "id": 625,
+        "name": "Carioca - 2",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/625.png"
+    },
+    {
+        "id": 475,
+        "name": "Paulista - A1",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/475.png"
+    },
+    {
+        "id": 477,
+        "name": "Gaúcho - 1",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/477.png"
+    },
+    {
+        "id": 629,
+        "name": "Mineiro - 1",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/629.png"
+    },
+    {
+        "id": 613,
+        "name": "Baiano - 2",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/613.png"
+    },
+    {
+        "id": 603,
+        "name": "Paraibano",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/603.png"
+    },
+    {
+        "id": 606,
+        "name": "Paranaense - 1",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/606.png"
+    },
+    {
+        "id": 616,
+        "name": "Potiguar",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/616.png"
+    },
+    {
+        "id": 604,
+        "name": "Catarinense - 1",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/604.png"
+    },
+    {
+        "id": 607,
+        "name": "Roraimense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/607.png"
+    },
+    {
+        "id": 620,
+        "name": "Cearense - 2",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/620.png"
+    },
+    {
+        "id": 75,
+        "name": "Serie C",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/75.png"
+    },
+    {
         "id": 624,
         "name": "Carioca - 1",
         "type": "League",
         "logo": "https://media.api-sports.io/football/leagues/624.png"
+    },
+    {
+        "id": 478,
+        "name": "Gaúcho - 2",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/478.png"
     },
     {
         "id": 475,
@@ -100,5 +304,166 @@ export const bestLeagues = [
         "name": "Copa do Nordeste",
         "type": "Cup",
         "logo": "https:\/\/media.api-sports.io\/football\/leagues\/612.png"
-      }
+      },
+      {
+        "id": 12,
+        "name": "CAF Champions League",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/12.png"
+    },
+      {
+        "id": 602,
+        "name": "Baiano - 1",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/602.png"
+    },
+    {
+        "id": 608,
+        "name": "Maranhense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/608.png"
+    },
+    {
+        "id": 605,
+        "name": "Paulista - A3",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/605.png"
+    },
+    {
+        "id": 609,
+        "name": "Cearense - 1",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/609.png"
+    },
+    {
+        "id": 610,
+        "name": "Brasiliense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/610.png"
+    },
+    {
+        "id": 611,
+        "name": "Capixaba",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/611.png"
+    },
+    {
+        "id": 614,
+        "name": "Paranaense - 2",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/614.png"
+    },
+    {
+        "id": 615,
+        "name": "Rondoniense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/615.png"
+    }, {
+        "id": 617,
+        "name": "Copa do Brasil U20",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/617.png"
+    },
+    {
+        "id": 618,
+        "name": "São Paulo Youth Cup",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/618.png"
+    },
+    {
+        "id": 619,
+        "name": "Mineiro - 2",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/619.png"
+    },
+    {
+        "id": 621,
+        "name": "Piauiense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/621.png"
+    },
+    {
+        "id": 622,
+        "name": "Pernambucano - 1",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/622.png"
+    },
+    {
+        "id": 623,
+        "name": "Sul-Matogrossense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/623.png"
+    },
+    {
+        "id": 626,
+        "name": "Sergipano",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/626.png"
+    },
+    {
+        "id": 627,
+        "name": "Paraense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/627.png"
+    },   
+    {
+        "id": 628,
+        "name": "Goiano - 1",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/628.png"
+    },
+    {
+        "id": 630,
+        "name": "Matogrossense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/630.png"
+    },
+    {
+        "id": 631,
+        "name": "Tocantinense",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/631.png"
+    },
+    {
+        "id": 632,
+        "name": "Supercopa do Brasil",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/632.png"
+    },
+    {
+        "id": 740,
+        "name": "CBF Brasileiro U20",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/740.png"
+    },
+    {
+        "id": 741,
+        "name": "Brasileiro de Aspirantes",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/741.png"
+    },
+    {
+        "id": 742,
+        "name": "Copa Paulista",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/742.png"
+    },
+    {
+        "id": 843,
+        "name": "Copa Verde",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/843.png"
+    },
+    {
+        "id": 851,
+        "name": "Carioca A2",
+        "type": "League",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/851.png"
+    },
+    {
+        "id": 805,
+        "name": "Copa Centroamericana",
+        "type": "Cup",
+        "logo": "https:\/\/media.api-sports.io\/football\/leagues\/805.png"
+    },
 ]
