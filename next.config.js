8fde4bba669e28d9889b603a321d8ca80b2f1684
Lucas Almeida <lucasnopc@gmail.com>
2022-04-07 11:05:17 -0300

    nextjs config module PWA complete

--- next.config.js
+++ next.config.js
@@ -1,17 +1,25 @@
 const withPWA = require('next-pwa')
 
+// module.exports = withPWA({
+//   pwa: {
+//     dest: 'public'
+//   }
+// })
+
+
 module.exports = withPWA({
-  pwa: {
-    dest: 'public'
-  }
-})
-
-
-module.exports = {
   images: {
     domains: ['media.api-sports.io'],
   },
-    env: {
-      customKey: 'my-value',
-    },
-  }
+  env: {
+    customKey: 'my-value',
+  },
+  pwa: {
+        dest: 'public'
+      }
+  // pwa: withPWA({
+  //   pwa: {
+  //     dest: 'public'
+  //   }
+  // })
+})
